fdd79fb11d2b9bfa2365f7353711b81abc4b73b1
Krittin Chuaiphikroh <krittin@42bangkok.com>
2024-09-16 23:00:10 +0900

    Fixup! Make it work with ESP32 interrupt
    Change-Id: Id07ef85700311adcd7f7d127775a8e7ebbcb79e4
    Signed-off-by: Krittin Chuaiphikroh <krittin@42bangkok.com>

--- BalanceRobot.ino
+++ BalanceRobot.ino
@@ -1,6 +1,7 @@
 #include <I2Cdev.h>						 // Provided by MPU6050
 #include <MPU6050_6Axis_MotionApps20.h>	 // MPU6050 v1.3.1 by Electronic Cats
 #include <PID_v1.h>						 // PID v.1.2.0 by Brett Beauregard
+// i2c dev lib
 
 #include "LMotorController.h"  // Custom
 #include "Wire.h"
@@ -37,16 +38,19 @@ PID pid(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
 double motorSpeedFactorLeft = 0.6;
 double motorSpeedFactorRight = 0.5;
 
-// MOTOR CONTROLLER
-#define I2C_SDA 33
-#define I2C_SCL 32
+// The necessary pins
+//  MOTOR CONTROLLER
+#define I2C_SDA 21
+#define I2C_SCL 22
 // To be edited
-#define ENA 5
-#define IN1 6
-#define IN2 7
-#define IN3 9
-#define IN4 8
-#define ENB 10
+#define ENA 15
+#define IN1 2
+#define IN2 4
+#define IN3 19
+#define IN4 18
+#define ENB 5
+//
+#define INTERRUPT_PIN 26
 LMotorController motorController(ENA, IN1, IN2, ENB, IN3, IN4, motorSpeedFactorLeft,
 								 motorSpeedFactorRight);
 
@@ -59,10 +63,12 @@ void dmpDataReady()
 void setup()
 {
 	// join I2C bus (I2Cdev library doesn't do this automatically)
-	Wire.begin(I2C_SDA, I2C_SCL, 6000000);	// Init at 6Mhz for 240 MHz CPU clock
+	Wire.begin();
+	Wire.setClock(400000);
 	Serial.begin(115200);
 
 	mpu.initialize();
+	pinMode(INTERRUPT_PIN, INPUT);
 
 	devStatus = mpu.dmpInitialize();
 
@@ -75,14 +81,24 @@ void setup()
 	// make sure it worked (returns 0 if so)
 	if (devStatus == 0)
 	{
+		// Calibration and generate offsets for accuracy
+		mpu.CalibrateAccel(6);
+		mpu.CalibrateGyro(6);
+		mpu.PrintActiveOffsets();
+
 		// turn on the DMP, now that it's ready
+		Serial.println(F("Enabling DMP..."));
 		mpu.setDMPEnabled(true);
 
 		// enable Arduino interrupt detection
-		attachInterrupt(0, dmpDataReady, RISING);
+		Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+		Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+		Serial.println(F(")..."));
+		attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
 		mpuIntStatus = mpu.getIntStatus();
 
 		// set our DMP Ready flag so the main loop() function knows it's okay to use it
+		Serial.println(F("DMP ready! Waiting for first interrupt..."));
 		dmpReady = true;
 
 		// get expected DMP packet size for later comparison
@@ -92,6 +108,9 @@ void setup()
 		pid.SetMode(AUTOMATIC);
 		pid.SetSampleTime(10);
 		pid.SetOutputLimits(-255, 255);
+		Serial.print(F("DMP Initialization (code "));
+		Serial.print(devStatus);
+		Serial.println(F(")"));
 	}
 	else
 	{
@@ -107,13 +126,16 @@ void setup()
 
 void loop()
 {
+	Serial.println("Restart!");
 	// if programming failed, don't try to do anything
 	if (!dmpReady)
 		return;
 
 	// wait for MPU interrupt or extra packet(s) available
-	while (!mpuInterrupt && fifoCount < packetSize)
+	Serial.println("loop 1");
+	while (!(mpu.dmpPacketAvailable()))
 	{
+		Serial.println("loop 2");
 		// no mpu data - performing PID calculations and output to motors
 		pid.Compute();
 		motorController.move(output, MIN_ABS_SPEED);
@@ -124,32 +146,10 @@ void loop()
 	mpuIntStatus = mpu.getIntStatus();
 
 	// get current FIFO count
-	fifoCount = mpu.getFIFOCount();
+	fifoCount = mpu.dmpGetCurrentFIFOPacket(fifoBuffer);
 
-	// check for overflow (this should never happen unless our code is too inefficient)
-	if ((mpuIntStatus & 0x10) || fifoCount == 1024)
-	{
-		// reset so we can continue cleanly
-		mpu.resetFIFO();
-		Serial.println(F("FIFO overflow!"));
-
-		// otherwise, check for DMP data ready interrupt (this should happen frequently)
-	}
-	else if (mpuIntStatus & 0x02)
-	{
-		// wait for correct available data length, should be a VERY short wait
-		while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-
-		// read a packet from FIFO
-		mpu.getFIFOBytes(fifoBuffer, packetSize);
-
-		// track FIFO count here in case there is > 1 packet available
-		// (this lets us immediately read more without waiting for an interrupt)
-		fifoCount -= packetSize;
-
-		mpu.dmpGetQuaternion(&q, fifoBuffer);
-		mpu.dmpGetGravity(&gravity, &q);
-		mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-		input = ypr[1] * 180 / M_PI + 180;
-	}
+	mpu.dmpGetQuaternion(&q, fifoBuffer);
+	mpu.dmpGetGravity(&gravity, &q);
+	mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+	input = ypr[1] * 180 / M_PI + 180;
 }
